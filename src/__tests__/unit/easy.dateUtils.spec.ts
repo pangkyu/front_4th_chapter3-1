9086505d839ed1532f41f049f7795c2f1cb7ad70
Bae seong-kyu <baesense@naver.com>
2025-02-02 13:10:43 +0900

    Test : getDaysInMonth

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -12,15 +12,32 @@ import {
 } from '../../utils/dateUtils';
 
 describe('getDaysInMonth', () => {
-  it('1월은 31일 수를 반환한다', () => {});
+  it('1월은 31일 수를 반환한다', () => {
+    expect(getDaysInMonth(2024, 1)).toBe(31);
+  });
 
-  it('4월은 30일 일수를 반환한다', () => {});
+  it('4월은 30일 일수를 반환한다', () => {
+    expect(getDaysInMonth(2024, 4)).toBe(30);
+  });
 
-  it('윤년의 2월에 대해 29일을 반환한다', () => {});
+  it('윤년의 2월에 대해 29일을 반환한다', () => {
+    expect(getDaysInMonth(2028, 2)).toBe(29);
+    expect(getDaysInMonth(2024, 2)).toBe(29);
+    expect(getDaysInMonth(2020, 2)).toBe(29);
+  });
 
-  it('평년의 2월에 대해 28일을 반환한다', () => {});
+  it('평년의 2월에 대해 28일을 반환한다', () => {
+    expect(getDaysInMonth(2021, 2)).toBe(28);
+    expect(getDaysInMonth(2022, 2)).toBe(28);
+    expect(getDaysInMonth(2023, 2)).toBe(28);
+    expect(getDaysInMonth(2025, 2)).toBe(28);
+    expect(getDaysInMonth(2026, 2)).toBe(28);
+  });
 
-  it('유효하지 않은 월에 대해 적절히 처리한다', () => {});
+  it('유효하지 않은 월에 대해 적절히 처리한다', () => {
+    expect(() => getDaysInMonth(2024, 0)).toThrow('유효하지 않은 월입니다.');
+    expect(() => getDaysInMonth(2024, 13)).toThrow('유효하지 않은 월입니다.');
+  });
 });
 
 describe('getWeekDates', () => {
