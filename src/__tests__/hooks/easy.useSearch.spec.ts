8a66a75a4c08d0046aed1194863f7b08f2c57233
Bae seong-kyu <baesense@naver.com>
2025-02-07 00:10:37 +0900

    Test : useSearch

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -3,12 +3,76 @@ import { act, renderHook } from '@testing-library/react';
 import { useSearch } from '../../hooks/useSearch.ts';
 import { Event } from '../../types.ts';
 
-it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {});
+const event: Event[] = [
+  {
+    id: '1',
+    title: '기존 회의',
+    date: '2024-10-15',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 10,
+  },
+  {
+    id: '2',
+    title: '기존 회의',
+    date: '2024-10-15',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'weekly', interval: 0 },
+    notificationTime: 10,
+  },
+  {
+    id: '3',
+    title: '점심',
+    date: '2024-10-15',
+    startTime: '09:00',
+    endTime: '10:00',
+    description: '기존 팀 미팅',
+    location: '회의실 B',
+    category: '업무',
+    repeat: { type: 'weekly', interval: 0 },
+    notificationTime: 10,
+  },
+];
 
-it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
+it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(event, new Date(), 'week'));
+  expect(result.current.searchTerm).toBe('');
+});
 
-it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {});
+it('검색어에 맞는 이벤트만 필터링해야 한다', () => {
+  const { result } = renderHook(() => useSearch(event, new Date(), 'week'));
+  act(() => {
+    result.current.setSearchTerm('기존 회의');
+  });
+  expect(result.current.filteredEvents).toEqual([]);
+});
 
-it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {});
+it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(event, new Date('2024-10-15'), 'week'));
+  act(() => {
+    result.current.setSearchTerm('회의실');
+  });
+  expect(result.current.filteredEvents).toHaveLength(2);
+  expect(result.current.filteredEvents).toEqual([event[0], event[1]]);
+});
 
-it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {});
+it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(event, new Date('2024-10-15'), 'week'));
+  expect(result.current.filteredEvents).toEqual([event[0], event[1]]);
+});
+
+it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
+  const { result } = renderHook(() => useSearch(event, new Date('2024-10-15'), 'week'));
+  act(() => {
+    result.current.setSearchTerm('점심');
+  });
+  expect(result.current.filteredEvents).toEqual([event[2]]);
+});
